78a41249a6db689c92f1a7c0c089a44d69c2db99
Stéphane Francel <contact@avine.io>
2017-05-25 00:36:21 +0200

    Improve colorize.js and set 3 colors

---
+++ colorize.js
@@ -0,0 +1,30 @@
+
+const colors = [
+  { value: '#337ab7', name: 'blue' },
+  { value: '#f0ad4e', name: 'yellow' },
+  { value: '#d9534f', name: 'red' }
+];
+
+const fs = require('fs');
+
+const srcPath = './src/logo/';
+
+const suffixes = ['icon', 'logo', 'sprite'];
+
+colors.forEach(color => {
+  suffixes.forEach(suffix => {
+    const srcFile = `${srcPath}avine-${suffix}.svg`;
+
+    let content = fs.readFileSync(srcFile, 'utf8');
+
+    content = content.replace(/#FFFFFF/g, color.value);
+
+    const targetFile = `${srcPath}colors/avine-${suffix}-${color.name}.svg`;
+
+    console.log(targetFile);
+
+    fs.writeFileSync(targetFile, content, 'utf8');
+  });
+
+  console.log('');
+});
